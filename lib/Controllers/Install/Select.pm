ca7453e29b84565b6a917b7568eafd6acb0e34e2
nathanIL <aloha2004@gmail.com.com>
2014-01-09 00:22:51 +0200

    Questions Parser

--- lib/Controllers/Install/Select.pm
+++ lib/Controllers/Install/Select.pm
@@ -1,10 +1,10 @@
 package Controllers::Install::Select;
 use Mojo::Base 'Mojolicious::Controller';
 
-# This action will render a template
+
+
 sub type {
   my $self = shift;
-
 
   $self->render( template => 'views/install/select' );
 }
